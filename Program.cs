68fcafa6e49e6af680dc6bf4cab389355a4b0810
Amanda Gawlowicz <agawlowicz07@gmail.com>
2022-03-23 10:56:07 -0500

    made from command line

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This was made from the command line.");
+        }
+    }
+}
